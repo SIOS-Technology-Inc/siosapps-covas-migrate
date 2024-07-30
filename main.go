5375669f4f631933b200b012cfefd2514a25ee7b
m-shibuya <m-shibuya@sios.com>
2024-07-30 09:48:55 +0900

    Feature: バージョンコードを修正

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	app := &cli.App{
 		Name:    "migrate",
 		Usage:   "MongoDB migration tool with minimal api",
-		Version: "0.6.0",
+		Version: "0.6.1",
 		Commands: []*cli.Command{
 			{
 				Name:  "init",
